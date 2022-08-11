eca848ddc99f7f45ac7f999e91de931eccd29dfe
BryanRJR <bryanrantung98@gmail.com>
2022-08-11 18:52:52 +0700

    make fragment home

--- app/src/main/java/com/bryanrantung/moneytrack/MainActivity.kt
+++ app/src/main/java/com/bryanrantung/moneytrack/MainActivity.kt
@@ -2,10 +2,56 @@ package com.bryanrantung.moneytrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.bryanrantung.moneytrack.databinding.ActivityMainBinding
+import com.bryanrantung.moneytrack.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.menu_main_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.menu_main_wishlist -> {
+                    true
+                }
+                R.id.menu_main_profile -> {
+                    true
+                }
+                R.id.menu_main_order -> {
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // replace fragment
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_main, fragment)
+            .commit()
+    }
+
+    // stop fragment
+    private fun stopFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .remove(fragment)
+            .commit()
     }
 }
